dd8e828e59af598411cb0be20e8aa9e13c0cd925
Faisal Islam <faisalislam@Faisals-Mac-mini.local>
2023-11-04 06:52:10 +0600

    solved repair bst in another way

--- bst/RepairBST.js
+++ bst/RepairBST.js
@@ -1,4 +1,36 @@
-function repairBst(tree){
+// function repairBst(tree) {
+//   let nodeOne = null;
+//   let nodeTwo = null;
+//   let previousNode = null;
+
+//   function inOrderTraversal(node){
+//     if(node === null) return; 
+    
+//     inOrderTraversal(node.left);
+
+//     if(previousNode !== null && node.value < previousNode.value){
+//       if(nodeOne === null){
+//         nodeOne = previousNode;
+//         nodeTwo = node;
+//       }else{
+//         nodeTwo = node;
+//       }
+//     }
+//     previousNode = node;
+//     inOrderTraversal(node.right);
+//   }
+
+//   inOrderTraversal(tree);
+
+//   const temp = nodeOne.value;
+//   nodeOne.value = nodeTwo.value;
+//   nodeTwo.value = temp;
+
+//   return tree;
+// }
+
+
+function repairBst(tree) {
   let nodeOne = null;
   let nodeTwo = null;
   let previousNode = null;
@@ -6,68 +38,32 @@ function repairBst(tree){
   let stack = [];
   let currentNode = tree;
 
-  while(true){
-    if(currentNode === null){
+  while (stack.length > 0 || currentNode !== null) {
+    if (currentNode !== null) {
+      stack.push(currentNode);
+      currentNode = currentNode.left;
+    } else {
       currentNode = stack.pop();
-      if(previousNode !== null && currentNode.value < previousNode.value){
-        if(!nodeOne){
+      if (previousNode !== null && currentNode.value < previousNode.value) {
+        if (!nodeOne) {
           nodeOne = previousNode;
           nodeTwo = currentNode;
-        }else{
-          nodeTwo = currentNode
+        } else {
+          nodeTwo = currentNode;
         }
       }
       previousNode = currentNode;
       currentNode = currentNode.right;
-    }else{
-      stack.push(currentNode);
-      currentNode = currentNode.left;
-    }
-
-    if(stack.length === 0 && currentNode === null) break;
-  }
-
-  const temp = nodeOne.value;
-  nodeOne.value = nodeTwo.value;
-  nodeTwo.value = temp;
-}
-
-const tree = {
-  value:10,
-  left:{
-    value:7,
-    left:{
-      value:3,
-      left:{
-        value:2,
-        left:null,
-        right:null
-      },
-      right:null
-    },
-    right:{
-      value:12,
-      left:null,
-      right:null
-    }
-  },
-  right:{
-    value:20,
-    left:{
-      value:8,
-      left:null,
-      right:{
-        value:14,
-        left:null,
-        right:null
-      }
-    },
-    right:{
-      value:22,
-      left:null,
-      right:null
     }
   }
+
+  // Swap the values of nodeOne and nodeTwo
+  if (nodeOne !== null && nodeTwo !== null) {
+    [nodeOne.value, nodeTwo.value] = [nodeTwo.value, nodeOne.value];
+  }
+
+  return tree;
 }
 
-console.log(repairBst(tree));
+
+
