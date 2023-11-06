724a29b3d8ff70483e8a51118171b8c3bb7d2cc0
Faisal Islam <faisalislam@Faisals-Mac-mini.local>
2023-11-06 20:34:47 +0600

    validate three nodes

---
+++ bst/ValidateThreeNodes.js
@@ -0,0 +1,18 @@
+function validateThreeNodes(nodeOne, nodeTwo, nodeThree) {
+  // Write your code here.
+  if(isDescendant(nodeTwo, nodeOne)) return isDescendant(nodeThree, nodeTwo);
+
+  if(isDescendant(nodeTwo, nodeThree)) return isDescendant(nodeOne, nodeTwo);
+
+  return false;
+}
+
+function isDescendant(node,target){
+  if(node === null) return false;
+
+  if(node === target) return true;
+
+  return target.value < node.value 
+    ? isDescendant(node.left, target) 
+    : isDescendant(node.right, target)
+}
